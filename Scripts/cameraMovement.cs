ed5dfe5197a462285d5ea42beae820ca43bb24f7
snoobler44 <salhab.anthony@yahoo.com>
2021-03-03 17:18:31 -0800

    Add files via upload

---
+++ Scripts/cameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+
+	public Transform player;
+
+	// Update is called once per frame
+	void Update() {
+		transform.position = player.transform.position;
+	}
+
+}
